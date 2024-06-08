e654ce594ddd690af64376bc402f263cb527b7a6
AngeloVienibene <vienibeneangelo@googlemail.com>
2024-06-08 18:55:35 +0200

    altes </tr> rausgemommen

--- Code Frontend/fe_memberlist.php
+++ Code Frontend/fe_memberlist.php
@@ -37,7 +37,7 @@
                     if ($result !== false && $result->num_rows > 0) {
                         // Display the table header and iterate through the fetched results
                         echo "<table id='table_memberlist'>"; 
-                        echo "<tr><th>Member ID</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Phone</th></tr><tr><th>Action</th></tr>";
+                        echo "<tr><th>Member ID</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Phone</th><th>Action</th></tr>";
                         while ($row = $result->fetch_assoc()) {
                             echo "<tr>";
                                 echo "<td>" . $row["member_id"] . "</td>";
