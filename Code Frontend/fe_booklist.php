9ebaa8f52e0a54deddc589f813f53ccc1c034f5e
Jules2543 <julia.sadlo.js@gmail.com>
2024-06-16 08:24:40 +0200

    booklist house button Farbe geändert

--- Code Frontend/fe_booklist.php
+++ Code Frontend/fe_booklist.php
@@ -135,8 +135,8 @@
     </div>
     <div class="sidebar"> <!-- adding sidebar, buttons and links -->
         <div class="buttons">
-        <button class="button_house"id="button_houseID"onclick="window.location.href='fe_dashboard.php'">
-                <i class="fa-solid fa-house" style="color: #0f0f0f;"></i> <!-- adding fontawesome icon -->
+            <button class="button_house"id="button_houseID"onclick="window.location.href='fe_dashboard.php'">
+                <i class="fa-duotone fa-house" style="--fa-primary-color: #000000; --fa-secondary-color: #000000; --fa-secondary-opacity: 0.7;"></i> <!-- adding fontawesome icon -->
             </button>
             <button class="button_equals"onclick="toggleMenu()">
                 <i class="fa-solid fa-bars"></i> <!-- adding fontawesome icon -->
